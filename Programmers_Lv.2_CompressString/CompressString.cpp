83a407a41aed8685abd750d74a26407b814f2f18
CalebHong <sstotss@naver.com>
2022-08-06 17:36:12 +0900

    Programmers_Lv.2_CompressString

--- Programmers_Lv.2_CompressString/CompressString.cpp
+++ Programmers_Lv.2_CompressString/CompressString.cpp
@@ -6,6 +6,7 @@
     URL : https://school.programmers.co.kr/learn/courses/30/lessons/60057
     * 문제: 압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여
            표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
+    * 예제 입력: "aabbaccc", "ababcdcdababcdcd", "abcabcdede", "abcabcabcabcdededededede", "xababcdcdababcdcd"
 */
 
 using namespace std;
