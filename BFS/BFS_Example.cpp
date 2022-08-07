a57ca4651f843c3ce73547ea24020d0c63daec70
CalebHong <sstotss@naver.com>
2022-08-07 23:44:23 +0900

    Update BFS_Example.cpp

--- BFS/BFS_Example.cpp
+++ BFS/BFS_Example.cpp
@@ -52,6 +52,7 @@ int main()
     cout << "BFS Algorithm!" << endl;
     pQueue->push(0);
     BFS(nodes, pQueue);
+    // Result: [0] [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11] [12] [13] [14] [15] [16] [17] [18] [19] [20]
 
     return 0;
 }
